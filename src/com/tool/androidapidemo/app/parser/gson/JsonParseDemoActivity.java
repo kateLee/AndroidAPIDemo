e585d9c660b225d74307cec45c8ea13dcc614a25
Tacowu <taco.wu@gmail.com>
2013-05-22 10:33:37 +0800

    add native json parse demo

---
+++ src/com/tool/androidapidemo/app/parser/gson/JsonParseDemoActivity.java
@@ -0,0 +1,17 @@
+package com.tool.androidapidemo.app.parser.gson;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tacowu
+ * Date: 5/22/13
+ * Time: 10:19 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JsonParseDemoActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
